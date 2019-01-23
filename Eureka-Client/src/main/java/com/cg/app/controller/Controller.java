bde34935c62ceab04007e60388714746b4f7926c
Rajendra Babu <kappala.rajendra-babu@capgemini.com>
2019-01-23 09:59:10 +0530

    Adds Eureka Server

--- Eureka-Client/src/main/java/com/cg/app/controller/Controller.java
+++ Eureka-Client/src/main/java/com/cg/app/controller/Controller.java
@@ -1,15 +1,20 @@
 package com.cg.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class Controller {
 
+	
+	
 	@GetMapping
-	public String helloworld() {
-		
-		return "hello world";
+	public String hello() {
+		//System.out.println(this.discoveryClient.getInstances("helloworld-service"));
+		return "Hello World!!!";
 	}
 }
--- Eureka-Website/src/main/java/com/app/web/controller/WebsiteController.java
+++ Eureka-Website/src/main/java/com/app/web/controller/WebsiteController.java
@@ -3,30 +3,30 @@ package com.app.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
-@RestController
+@Controller
 public class WebsiteController {
-	
-	 
-	 
-	 @Autowired
-	 private RestTemplate restTemplate;
 
-	 @RequestMapping
-		public String helloworld(Model model) {
-			
-			  String output = restTemplate .exchange("http://first", HttpMethod.GET, null,
-			  new ParameterizedTypeReference<String>() { }).getBody();
-			 
-			 model.addAttribute("Hello", output); 
-			
-			return "demo";
-		}
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping
+	public String helloworld(Model model) {
+		
+		  String output = restTemplate .exchange("http://first", HttpMethod.GET, null,
+		  new ParameterizedTypeReference<String>() { }).getBody();
+		 
+		 model.addAttribute("Hello", output); 
+		
+		return "helloworld";
+	}
 
 }
